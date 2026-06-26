24f55a5a83b32198d579488686046864211be65e
Julian V <j.vilfort01@gmail.com>
2026-06-26 16:34:00 -0400

    Add skills

---
+++ skills.sh
@@ -0,0 +1,6 @@
+npx skills add https://github.com/anthropics/claude-code.git --skill frontend-design -y && \
+npx skills add https://github.com/shadcn-ui/ui.git -y && \
+npx skills add https://github.com/bergside/awesome-design-skills.git --skill dashboard -y && \
+npx skills add https://github.com/nextlevelbuilder/ui-ux-pro-max-skill.git -y && \
+npx skills add https://github.com/greensock/gsap-skills.git -y && \
+npx skills add https://github.com/leonxlnx/taste-skill --skill minimalist-ui -y
